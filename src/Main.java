37ded0a7b9cdc9347bbd2adab7c91b15e239a646
Ridwan <ridwanur196@gmail.com>
2025-03-06 14:50:29 +0600

    multiple command feature added

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         Scanner scanner = new Scanner(System.in);
 
         if (args.length == 1) {
-            System.out.print(args[0] + "> ");
-            String command = scanner.nextLine();
-
-            String os = System.getProperty("os.name").toLowerCase();
-            Process process;
-
-            if(os.contains("win")) {
-                if(command.equals("ls")){
-                    command = "dir";
+            while (true) {
+                System.out.print(args[0] + "> ");
+                String command = scanner.nextLine();
+                if(Objects.equals(command, "quit")){
+                    break;
                 }
-                process = Runtime.getRuntime().exec("cmd.exe /c " + command);
-            } else {
-                process = Runtime.getRuntime().exec(command);
-            }
+                String os = System.getProperty("os.name").toLowerCase();
+                Process process;
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                if (os.contains("win")) {
+                    if (command.equals("ls")) {
+                        command = "dir";
+                    } else if (command.equals("pwd")) {
+                        command = "cd";
+                    }
+                    process = Runtime.getRuntime().exec("cmd.exe /c " + command);
+                } else {
+                    process = Runtime.getRuntime().exec(command);
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                reader.close();
             }
-            reader.close();
         } else {
             System.out.println("No command line arguments found.");
         }
